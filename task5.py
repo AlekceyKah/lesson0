4e5d691f2f8b1b666b99202efb88e8640b52c8cd
AlekceyKah <alekcey.v@gmail.com>
2025-01-13 02:32:12 +0300

    Update task5.py

--- task5.py
+++ task5.py
@@ -1,7 +1,7 @@
 # "4th program"
 nem_str = '123.456' # string  строка
 nem_str = float (nem_str)# converting преобразование в дробное число
-print(int(nem_str * 10) % 10) #
+print(int(nem_str * 10) % 10) 
 '''
  * математические действия,
  converting преобразование в целое число,
